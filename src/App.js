3d7eb4988468d91e635d7d4fb7974164f153d432
zhoujin <zhoujin@iboxpay.com>
2018-11-19 18:26:01 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import AddTodo from './components/Todo/views/addTodo'
+import { view as Todos } from './components/Todo'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <AddTodo />
+        <Todos></Todos>
       </div>
     );
   }
